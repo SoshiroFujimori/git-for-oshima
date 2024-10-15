13abd41d47954fe2e816d1c453e67bd18d7952b2
soshiroFujimori <oshima.k.kondate@gmail.com>
2024-10-15 18:16:53 +0900

    setup.batで依存ファイルの存在を確認し、未存在時にのみダウンロードを実行するように修正

--- setup.bat
+++ setup.bat
@@ -6,11 +6,15 @@ if not exist deps mkdir deps
 
 :: Download 7zr.exe
 echo Downloading: 7zr.exe
-bitsadmin /transfer "7zrDownload" https://www.7-zip.org/a/7zr.exe %cd%\deps\7zr.exe
+if not exist deps\7zr.exe (
+    bitsadmin /transfer "7zrDownload" https://www.7-zip.org/a/7zr.exe %cd%\deps\7zr.exe
+)
 
 :: Download PortableGit-2.47.0-64-bit.7z.exe
 echo Downloading: PortableGit-2.47.0-64-bit.7z.exe
-bitsadmin /transfer "gitDownload" https://github.com/git-for-windows/git/releases/download/v2.47.0.windows.1/PortableGit-2.47.0-64-bit.7z.exe %cd%\deps\PortableGit-2.47.0-64-bit.7z.exe
+if not exist deps\PortableGit-2.47.0-64-bit.7z.exe (
+    bitsadmin /transfer "gitDownload" https://github.com/git-for-windows/git/releases/download/v2.47.0.windows.1/PortableGit-2.47.0-64-bit.7z.exe %cd%\deps\PortableGit-2.47.0-64-bit.7z.exe
+)
 
 :: Extract Git to deps folder
 echo Extracting Git...
