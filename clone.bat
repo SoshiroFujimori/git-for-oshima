70c517397013af297cdbe4eda270a7cc5c478a63
soshiroFujimori <oshima.k.kondate@gmail.com>
2025-10-06 12:13:03 +0900

    Enhance clone and sync scripts to support submodules and improve messaging

--- clone.bat
+++ clone.bat
@@ -10,11 +10,11 @@ set /p repoUrl="Please enter the Git repository URL to clone: "
 :: Move to the repos folder
 cd repos
 
-:: Clone the repository
-"../deps/git/bin/git.exe" clone %repoUrl%
+:: Clone the repository and its submodules
+"../deps/git/bin/git.exe" clone --recurse-submodules %repoUrl%
 
 :: Display a message when cloning is complete
-echo Repository clone completed.
+echo Repository and submodules clone completed.
 
 endlocal
 pause
--- sync.bat
+++ sync.bat
@@ -4,23 +4,31 @@ setlocal
 :: Move to the repos folder
 cd repos
 
-:: Traverse all subfolders and execute push
+:: Traverse all subfolders and execute sync
 for /d %%d in (*) do (
     echo Moving to repository %%d...
     cd %%d
     if exist .git (
-        echo Syncing repository %%d...
-        "../../deps/git/bin/git.exe" add .
-        "../../deps/git/bin/git.exe" commit -m "Automatic sync from sync.bat"
+        echo Syncing main repository %%d...
         "../../deps/git/bin/git.exe" pull --rebase
-        "../../deps/git/bin/git.exe" push
+        "../../deps/git/bin/git.exe" submodule update --init --recursive
+
+        echo.
+        echo Syncing submodules for %%d...
+        "../../deps/git/bin/git.exe" submodule foreach "git add . && (git commit -m \"Automatic sync from sync.bat\" || echo \"No changes to commit in submodule\") && git pull --rebase && git push"
+        
+        echo.
+        echo Committing submodule updates in %%d...
+        "../../deps/git/bin/git.exe" add .
+        (git commit -m "Update submodules" && git push) || echo "No submodule updates to commit in main repo."
+
     ) else (
         echo %%d is not a Git repository.
     )
     cd ..
 )
 
-echo All repositories have been synced.
+echo All repositories and submodules have been synced.
 
 endlocal
 pause
